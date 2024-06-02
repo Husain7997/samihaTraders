946a2e87c67eb5327eac5949fe08a870c6190907
husainahmed97 <68569532+husainahmed97@users.noreply.github.com>
2024-06-02 19:38:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <div>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+    </div>
+  )
+}
+export default App
